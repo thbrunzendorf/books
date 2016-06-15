09e4cf04dab45337a87215a8b055bcc05cb303b3
Thorsten <thbrunzendorf@aol.com>
2016-06-15 20:09:53 +0200

    first equal implementation passes test

--- src/main/java/de/thbrunzendorf/demo/books/BookRegisteredEvent.java
+++ src/main/java/de/thbrunzendorf/demo/books/BookRegisteredEvent.java
@@ -4,4 +4,11 @@ package de.thbrunzendorf.demo.books;
  * Created by Thorsten on 15.06.2016.
  */
 public class BookRegisteredEvent {
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {return false;}
+        if (this.getClass() != obj.getClass()) {return false;}
+        return true;
+    }
 }
--- src/test/java/de/thbrunzendorf/demo/books/BooksTest.java
+++ src/test/java/de/thbrunzendorf/demo/books/BooksTest.java
@@ -2,16 +2,27 @@ package de.thbrunzendorf.demo.books;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Thorsten on 15.06.2016.
  */
 public class BooksTest {
 
+    private List<BookRegisteredEvent> newEvents = new ArrayList<BookRegisteredEvent>();
 
     private void when(RegisterBookCommand command) {
+        BookRegisteredEvent event = new BookRegisteredEvent();
+        newEvents.add(event);
     }
 
     private void then(BookRegisteredEvent event) {
+        List<BookRegisteredEvent> expectedEvents = new ArrayList<BookRegisteredEvent>();
+        expectedEvents.add(event);
+        assertEquals(expectedEvents, newEvents);
     }
 
     @Test
