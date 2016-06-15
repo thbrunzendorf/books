357079d453c27ca456a9d21e83f2815a97953a71
Thorsten <thbrunzendorf@aol.com>
2016-06-15 19:43:44 +0200

    first test

--- src/test/java/de/thbrunzendorf/demo/books/BooksTest.java
+++ src/test/java/de/thbrunzendorf/demo/books/BooksTest.java
@@ -1,7 +1,16 @@
 package de.thbrunzendorf.demo.books;
 
+import org.junit.Test;
+
 /**
  * Created by Thorsten on 15.06.2016.
  */
 public class BooksTest {
+
+    @Test
+    public void bookRegistered() {
+
+    }
+
+
 }
